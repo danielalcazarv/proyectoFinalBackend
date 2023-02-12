533a59b0debc677b66646659fa47a6658029c222
Daniel Alcazar <danielalcazar54@gmail.com>
2023-02-12 03:38:28 -0300

    MOD nombre middleware mongoIdFail

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,17 +1,17 @@
 import {Router} from 'express';
 import ProductosController from '../controllers/productos.controller.js';
 import auth from '../middlewares/auth.middleware.js'
-import mongoFailGetById from '../middlewares/productos.middleware.js';
+import mongoFailGetById from '../middlewares/mongoIdFail.middleware.js';
 const controlador = new ProductosController()
 
 const routes = Router();
 
-routes.get('/', controlador.obtenerProductos);
-routes.get('/vista/:categoria',  controlador.obtenerCategorias)
-routes.get('/ofertas',  controlador.obtenerOfertas);
-routes.get('/detalle/:id', mongoFailGetById, controlador.obtenerProducto);
-routes.post('/admin/guardar', controlador.guardarProducto);
-routes.delete('/admin/borrar/:id', mongoFailGetById, controlador.borrarProducto);
-routes.put('/admin/actualizar/:id', mongoFailGetById, controlador.actualizarProducto);
+routes.get('/', auth, controlador.obtenerProductos);
+routes.get('/vista/:categoria', auth,  controlador.obtenerCategorias)
+routes.get('/ofertas', auth, controlador.obtenerOfertas);
+routes.get('/detalle/:id', auth, mongoFailGetById, controlador.obtenerProducto);
+routes.post('/admin/guardar', auth, controlador.guardarProducto);
+routes.delete('/admin/borrar/:id', auth, mongoFailGetById, controlador.borrarProducto);
+routes.put('/admin/actualizar/:id', auth,  mongoFailGetById, controlador.actualizarProducto);
 
 export default routes;
